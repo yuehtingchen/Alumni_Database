099115d22abbc70a9accccc722efb19192f590e5
yuehtingchen <alicetingchen@gmail.com>
2021-03-02 15:05:14 +0800

    add environment variables

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from config import Config
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'test.db')
-app.secret_key = b'\xd7\xdb\x8e8\xc2e\xdaT\xe5B\xe3\xd8\x1e\xce\xa5\x19'
+app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
